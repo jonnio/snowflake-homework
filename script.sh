2badbb8276dbf84108a54afd8408fe1f074320c3
Jon Osborn <josborn@belltracy.com>
2024-08-12 10:50:05 -0700

    Basic working version to send to snowflake

---
+++ script.sh
@@ -0,0 +1,14 @@
+
+
+gcloud auth login
+gcloud config set project snowflakehomework
+
+gcloud config set run/region us-east1
+
+gcloud auth configure-docker
+
+poetry export -f requirements.txt --output requirements.txt
+
+
+
+
