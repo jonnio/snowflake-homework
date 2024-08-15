ecba8e3de9e6240f000f68e5a2f0c119f1334b1f
Jon Osborn <josborn@belltracy.com>
2024-08-15 16:58:52 -0400

    remove unused code

--- ux/src/main/java/ai/osborn/snowflake/MainController.java
+++ ux/src/main/java/ai/osborn/snowflake/MainController.java
@@ -1,10 +1,8 @@
 package ai.osborn.snowflake;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,17 +12,10 @@ public class MainController {
 
     @Value("${homework.api.url}")
     private String apiBaseUrl;
-    @Value("${spring.security.oauth2.client.registration.github.client-id}")
-    private String clientId;
 
-    private final OAuth2AuthorizedClientRepository authorizedClientRepository;
-
-    public MainController(OAuth2AuthorizedClientRepository authorizedClientRepository) {
-        this.authorizedClientRepository = authorizedClientRepository;
-    }
 
     @GetMapping("/")
-    public String index(@RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient authorizedClient, HttpServletRequest request, Model model) {
+    public String index(@RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient authorizedClient, Model model) {
         model.addAttribute("apiBaseUrl", apiBaseUrl);
         model.addAttribute("module", "home");
         model.addAttribute("token", authorizedClient.getAccessToken().getTokenValue());
